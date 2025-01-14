3aff4b7a772f7d490fa246b3b53e7cc105fa58d1
Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-01-14 22:43:12 +0545

    sapphire: added COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_sapphire.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_sapphire-ap4a-user \
+    lineage_sapphire-ap4a-userdebug \
+    lineage_sapphire-ap4a-eng
